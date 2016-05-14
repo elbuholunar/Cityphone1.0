ef28ab9ec86e2289363fa3beaf0d513672cd572b
elbuholunar <elbuholunar@gmail.com>
2016-05-14 13:53:03 -0500

    fase 1 api
    creacion htacces, metodo de error y extracción automatica de metodo y
    params

--- API/apiconsulta.php
+++ API/apiconsulta.php
@@ -1,13 +1,54 @@
-<?php 
-	if(isset($_GET['user']) && intval($_GET['user'])) {
+<?php     
+    // Variable que contiene el json de respuesta
+    $err[0] = "Error en el request, consulte la documentación";
+    $url = str_replace("API/","",$_REQUEST['url']);        
+    $parts = explode("/", $url);
 
-	$format = 'json';
-        if($format == 'json') {
+    if ($_SERVER['REQUEST_METHOD'] != "POST" || count($parts) <= 0) { 
+        error($err[0],"400",array());        
+    }else {        
+        $metod = $parts[0]; 
+        if(count($parts)>1){
+            $subcadena = "/";         
+            $posicionsubcadena = strpos ($url, $subcadena);
+            $params = substr ($url, ($posicionsubcadena+1)); 
+             $metod(explode("/",$params));
+        } else {
+            $params = array();
+        }    
+        
+    }
+
+
+    function prueba($string1){
+        if(count($string1) != 2){
+            global $err;
+            error($err[0],"400",$string1);
+        }else{
             header('Content-type: application/json');
-            echo json_encode(array('success'=>"success",
-            						'prueba'=>htmlentities("¡Hola, porque tan bola!")
-            	));
-        }
+             echo json_encode(array('success'=>"success",
+                                'data'=> array("1"=>$string1),
+                                'message'=> htmlentities("¡Hola, porque tan bola!")
+            ));
+        }        
 
-	}
+    }
+
+    /**
+     *  Devulelve el error con el mensaje y código de error
+     *  @author Alvaro Bernal
+     *  @param {string} mensaje de error
+     *  @param {String} Numero de error
+     *  @param {Array} Datos a devolver
+     *  @return {Object Json}
+     */
+    function error($msg,$num,$data){
+        header('Content-type: application/json');
+        echo json_encode(array('success'=>"error",
+                                'data'=> $data,
+                                'message'=> htmlentities($msg),
+                                'error number'=>$num
+        ));
+    }
+	
  ?>
