5f150d4fff582f5bb7e910327d3e57e9b7edb049
MiguelCasvas <gepeto_1223@hotmail.com>
2016-05-14 09:23:42 -0500

    Edicion crearUsuario

--- views/users/crearUsuario.php
+++ views/users/crearUsuario.php
@@ -1,16 +1,21 @@
+<?php 
+  require_once ('../../libraries/php/Simple_sessions.php');
+  $obj_Session = new Simple_sessions();
+?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
-	<meta charset="utf-8"/> 
-	<link href='https://fonts.googleapis.com/css?family=Fjalla+One' rel='stylesheet' type='text/css'>
-	<meta name="viewport" content="width=device-width, initial-scale=1">	
-	<link href="../../css/css_bootstrap/bootstrap.css" rel="stylesheet" media="screen"> 
-	<link href="../../css/css_bootstrap/bootstrap-theme.css" rel="stylesheet" media="screen">
-	<script type="text/javascript" src="../../libraries/js/jquery/jquery-2.2.3.min.js"></script>
-	<script type="text/javascript" src="../../libraries/js/js_bootstrap/bootstrap.min.js"></script>
-	<script type="text/javascript" src="../../js/manageJsonConf.js"></script>
-	<link rel="stylesheet" type="text/css" href="../../css/users/principal.css">	
+  <title></title>
+  <meta charset="utf-8"/> 
+  <link href='https://fonts.googleapis.com/css?family=Fjalla+One' rel='stylesheet' type='text/css'>
+  <meta name="viewport" content="width=device-width, initial-scale=1">  
+  <link href="../../css/css_bootstrap/bootstrap.css" rel="stylesheet" media="screen"> 
+  <link href="../../css/css_bootstrap/bootstrap-theme.css" rel="stylesheet" media="screen">
+  <script type="text/javascript" src="../../libraries/js/jquery/jquery-2.2.3.min.js"></script>
+  <script type="text/javascript" src="../../libraries/js/js_bootstrap/bootstrap.min.js"></script>
+  <script type="text/javascript" src="../../js/manageJson.js"></script>
+  <link rel="stylesheet" type="text/css" href="../../css/users/principal.css">
+  
 </head>
 <body>
 <nav class="navbar navbar-default size-default">
@@ -51,18 +56,18 @@
         <input type="submit" class="btn btn-default changeLanguages" id="buttonSearch" value="Buscar">
       </form>
       <ul class="nav navbar-nav navbar-right">
-      	<li><a href="#"><img src="../../resources/images/Es.png" class="btn-languages" alt="es-co" id="uno"></a></li>
-      	<li><a href="#"><img src="../../resources/images/En.png" class="btn-languages" alt="en-us"></a></li>
-      	<li><a href="#"><img src="../../resources/images/Fr.png" class="btn-languages" alt="fr"></a></li>
-        <li><a href="../general/header.php" ><font id="logOut" class="changeLanguages">Link</font></a></li>
+        <li><a href="#"><img src="../../resources/images/Es.png" class="btn-languages" alt="es-co" id="uno"></a></li>
+        <li><a href="#"><img src="../../resources/images/En.png" class="btn-languages" alt="en-us"></a></li>
+        <li><a href="#"><img src="../../resources/images/Fr.png" class="btn-languages" alt="fr"></a></li>
+        <li><a href="../users/login.php" id="sesion" class="changeLanguages"><font id="sesion" class="changeLanguages">Link</font></a></li>
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"  ><font id="Configurations" class="changeLanguages">Dropdown</font> <span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="crearUsuario.php"><font id="createUser" class="changeLanguages">Action</font></a></li>
-            <li><a href="#"><font id="updateUser" class="changeLanguages">Another action</font></a></li>
-            <li><a href="#"><font id="showUsers" class="changeLanguages">Something else here</font></a></li>
+            <li><a href="#" id="createUser" class="changeLanguages">Action</a></li>
+            <li><a href="#" id="updateUser" class="changeLanguages">Another action</a></li>
+            <li><a href="#" id="showUsers" class="changeLanguages">Something else here</a></li>
             <li role="separator" class="divider"></li>
-            <li><a href="#"><font id="deleteUser" class="changeLanguages">Separated link</font></a></li>
+            <li><a href="#" id="deleteUser" class="changeLanguages">Separated link</a></li>
           </ul>
         </li>
       </ul>
@@ -70,100 +75,117 @@
   </div><!-- /.container-fluid -->
 </nav>
 <header>
-	<div class="header">
-		<div class="titutlo shadow-text">
-			<h1><font id="title" class="changeLanguages">SISTEMA DE CITOFONIA VIRTUAL</font></h1>
-			<h3><font id="subtitle" class="changeLanguages">Con el Respaldo de Estecnología</font></h3>
-		</div>
-		<div class="logo">
-			<img src="../../resources/images/logovp.png">
-		</div>		
-	</div>
-</header>
-	
-
-
-
-
-
-<div class="general-content">
-<h3 class="center-obj paddintop-20"><font id="title3" class="changeLanguages">CREAR NUEVO USUARIO</font></h3>
-	<div class="container-fluid" >   
-    <div class="row" >
-      <div class="col-md-1 col-xd-1"></div> 
-      <div class="col-md-10 col-xd-10 well">        
-        <form action="../../controllers/UsuarioController.php" class="form-horizontal" method="POST">
-          <div class="form-group">            
-              <label for="name" id="nameLabel" class="changeLanguages col-sm-2">Nombre:</label>
-              <div class="col-sm-10"><input type="text" name="name" id="name" placeholder="Nombre de Usuario" class="form-control changeLanguages"/> </div>         
-          </div>
-          <div class="form-group">            
-              <label for="apellido" id="apellidoLabel" class="col-sm-2 changeLanguages">Apellido:</label>
-              <div class="col-sm-10"><input type="text" name="apellido" id="apellido" placeholder="Nombre de Usuario" class="form-control changeLanguages"/>  </div>           
-          </div>
-          <div class="form-group">            
-              <label for="tipo" id="tipoLabel" class="col-sm-2 changeLanguages">Tipo de Usuario:</label>
-              <div class="col-sm-10">
-                <select name="tipo" id="tipo" placeholder="Tipo de Usuario" class="form-control changeLanguages"/>    
-                  <option value="1">Usuario</option>
-                  <option value="2">Editor</option>
-                  <option value="3">Adminstrador</option>
-                  <option value="4">Super Administrador</option>
-                </select> 
-              </div>        
-          </div>
-          <div class="form-group">            
-              <label for="userName" id="usernameLabel" class="col-sm-2 changeLanguages">Nombre Usuario:</label>
-              <div class="col-sm-10"><input type="text" name="username" id="username" placeholder="Nombre de Usuario" class="form-control changeLanguages"/>      </div>       
-          </div>
-          <div class="form-group">            
-              <label for="password" id="passwordLabel" class="col-sm-2 changeLanguages">Contraseña:</label>
-              <div class="col-sm-10"><input type="password" name="password" id="password" placeholder="Contraseña" class="form-control changeLanguages"/>     </div>        
-          </div>
-          <div class="form-group">            
-              <label for="password2" id="password2Label" class="col-sm-2 changeLanguages">Repita la Contraseña:</label>
-              <div class="col-sm-10"><input type="password" name="password2" id="password2" placeholder="Contraseña" class="form-control changeLanguages"/>  </div>           
-          </div>
-          <div class="form-group">            
-              <label for="email" id="emailLabel" class="col-sm-2 changeLanguages">Correo Electrónico:</label>
-              <div class="col-sm-10"><input type="text" name="email" id="email" placeholder="Contraseña" class="form-control changeLanguages"/>     </div>        
-          </div>
-          <div class="form-group">            
-              <label for="telFijo" id="telFijoLabel" class="col-sm-2 changeLanguages">Téléfono Fijo:</label>
-              <div class="col-sm-10"><input type="text" name="telFijo" id="telFijo" placeholder="Contraseña" class="form-control changeLanguages"/>     </div>        
-          </div>
-          <div class="form-group">            
-              <label for="telMovil" id="telMovilLabel" class="col-sm-2 changeLanguages">Teléfono Movil:</label>
-              <div class="col-sm-10"><input type="text" name="telMovil" id="telMovil" placeholder="Contraseña" class="form-control changeLanguages"/>     </div>        
-          </div>
-          <div class="form-group">            
-              <label for="text" id="estadoLabel" class="col-sm-2 changeLanguages">Estado:</label>
-              <div class="col-sm-10">
-                <select name="estado" id="estado" placeholder="Contraseña" class="form-control changeLanguages"/>   
-                  <option value="1">En Espera</option>                  
-                  <option value="2">Inactivo</option>
-                  <option value="3">Suspendido</option>
-                  <option value="4">Bloqueado</option>
-                  <option value="5">Cambio de Contraseña</option>
-                  <option value="6">Activo</option>
-                </select>   
-              </div>       
-          </div>
-          <div >        
-              <input type="hidden" name="event" id="event" value='{"action":"login"}' class="form-control"/>
-              <input type="submit" value="Crear Usuario" id="btnCrear" class="btn btn-default changeLanguages"/>     
-          </div>
-        </form>
-      </div>  
-      <div class="col-md-4 col-xd-1"></div> 
-    </div> 
+  <div class="header">
+    <div class="titutlo shadow-text">
+      <h1><font id="title" class="changeLanguages">SISTEMA DE CITOFONIA VIRTUAL</font></h1>
+      <h3><font id="subtitle" class="changeLanguages">Con el Respaldo de Estecnología</font></h3>
+    </div>
+    <div class="logo">
+      <img src="../../resources/images/logovp.png">
+    </div>    
   </div>
+</header>
+  
+
+
+
+
+
+<div class="general-content" style="padding: 25px 25px;">
+  <form action="../../controllers/UsuarioController.php" method="POST" role="form" method="post">
+    <legend>Creación Usuario</legend>
+    <!--FILA 1-->
+    <div class="row">
+        <div class="col-xs-2 col-sm-2 col-md-2 col-lg-3"></div>
+        <div class="form-group col-xs-12 col-sm-12 col-md-12 col-lg-3">
+            <label for="">Tipo Identificación</label>
+            <select name="tipoIdentificacion" id="inputTipoIdentificacion" class="form-control" required="required">
+              <option value="">Seleccinar</option>
+              <option value="1">Cedula</option>
+              <option value="2">Cedula Extranjera</option>
+              <option value="3">Tarjeta Identidad</option>
+            </select>
+        </div>
+        <div class="form-group col-xs-12 col-sm-12 col-md-12 col-lg-3">
+            <label for="">Identificacion</label>
+            <input type="text" name="identificacion" class="form-control" id="" placeholder="..." required="required">
+        </div>       
+        <div class="col-xs-2 col-sm-2 col-md-2 col-lg-3"></div>
+    </div>
+    <!--FILA 2-->
+    <div class="row">
+        <div class="col-xs-2 col-sm-2 col-md-2 col-lg-3"></div>
+        <div class="form-group col-xs-12 col-sm-12 col-md-12 col-lg-3">
+            <label for="">Nombres</label>
+            <input type="text" name="nombre" class="form-control" id="" placeholder="..." required="required">
+        </div>
+        <div class="form-group col-xs-12 col-sm-12 col-md-12 col-lg-3">
+            <label for="">Apellidos</label>
+            <input type="text" name="apellido" class="form-control" id="" placeholder="..." required="required">
+        </div>       
+        <div class="col-xs-2 col-sm-2 col-md-2 col-lg-3"></div>
+    </div>
+    <!--FILA 3-->
+    <div class="row">
+        <div class="col-xs-2 col-sm-2 col-md-2 col-lg-3"></div>
+        <div class="form-group col-xs-12 col-sm-12 col-md-12 col-lg-3">
+          <label for="">Telefono Fijo</label>
+          <input type="text" name="telFijo" class="form-control" value="" placeholder="..." required="required">
+        </div>
+        <div class="form-group col-xs-12 col-sm-12 col-md-12 col-lg-3">
+          <label for="">Telefono Movil</label>
+          <input type="text" name="telMovil" class="form-control" value="" placeholder="..." required="required">
+        </div>       
+        <div class="col-xs-2 col-sm-2 col-md-2 col-lg-3"></div>                
+    </div>
+    <!--FILA 4-->
+    <div class="row">
+        <div class="col-xs-2 col-sm-2 col-md-2 col-lg-3"></div>
+        <div class="form-group col-xs-12 col-sm-12 col-md-12 col-lg-3">
+            <label for="">Correo</label>
+            <input type="text" name="email" class="form-control" value="" placeholder="..." required="required"> 
+        </div>
+        <div class="form-group col-xs-12 col-sm-12 col-md-12 col-lg-3">
+            <label for="">Tipo de usuario</label>
+            <select name="tipoUsuario" id="input" class="form-control" required="required">
+              <option value="">Seleccinar</option>
+              <option value="1">Administrador</option>
+              <option value="2">usuario</option>
+            </select>
+        </div>       
+        <div class="col-xs-2 col-sm-2 col-md-2 col-lg-3"></div>                
+    </div>
+    <!--FILA 5-->
+    <div class="row">
+        <div class="col-xs-2 col-sm-2 col-md-2 col-lg-3"></div>
+        <div class="form-group col-xs-12 col-sm-12 col-md-12 col-lg-3">
+            <label for="">Estado</label>
+            <select name="estado" id="input" class="form-control" required="required">
+              <option value="">Seleccinar</option>
+              <option value="1">Estado 1</option>
+              <option value="2">Estado 2</option>
+              <option value="3">Estado 3</option>
+            </select>
+        </div>
+        <div class="form-group col-xs-12 col-sm-12 col-md-12 col-lg-3"></div>       
+        <div class="col-xs-2 col-sm-2 col-md-2 col-lg-3"></div>                
+    </div>
+    <!--FILA 5-->
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+          <input type="hidden" name="event" id="event" value='{"action":"crear"}' class="form-control"/>
+          <input type="submit" value="Registrar" class="btn btn-primary pull-right btn-lg btn-block">          
+        </div>                
+    </div>       
+  </form>
 </div>
+
+
 <footer>
-	<div class="footer">
-		
-	</div>
-	
+  <div class="footer">
+    
+  </div>
+  
 </footer>
 </body>
 </html>
